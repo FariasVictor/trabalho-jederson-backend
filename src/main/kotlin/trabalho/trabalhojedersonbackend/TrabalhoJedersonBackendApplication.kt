f27556befe6ee94d1d7c917f0c15440fab944b4e
idd_vsilva <idd_vsilva@uolinc.com>
2020-05-07 18:29:10 -0300

    first commit

---
+++ src/main/kotlin/trabalho/trabalhojedersonbackend/TrabalhoJedersonBackendApplication.kt
@@ -0,0 +1,11 @@
+package trabalho.trabalhojedersonbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrabalhoJedersonBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<TrabalhoJedersonBackendApplication>(*args)
+}
---
+++ src/test/kotlin/trabalho/trabalhojedersonbackend/TrabalhoJedersonBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package trabalho.trabalhojedersonbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrabalhoJedersonBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
