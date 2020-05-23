844df291377e973cf7795368777c1e019833bbce
idd_vsilva <idd_vsilva@uolinc.com>
2020-05-22 23:15:28 -0300

    Create PatientController(just findAll and findById)

---
+++ src/main/kotlin/trabalho/trabalhojedersonbackend/controller/PatientController.kt
@@ -0,0 +1,23 @@
+package trabalho.trabalhojedersonbackend.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import trabalho.trabalhojedersonbackend.model.Patient
+import trabalho.trabalhojedersonbackend.services.PatientService
+
+@RestController
+@RequestMapping("/patient")
+class PatientController(val patientService: PatientService) {
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): Patient {
+        return patientService.findById(id)
+    }
+
+    @GetMapping()
+    fun findAll(): List<Patient> {
+        return patientService.findAll();
+    }
+}
