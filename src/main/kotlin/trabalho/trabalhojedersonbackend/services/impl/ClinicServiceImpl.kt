415a9ff6d6ee1756d412c37cb398c0ffa7044324
Romney Pereira <s2it_rpereira@uolinc.com>
2020-06-05 01:52:31 -0300

    doctor : doctor pronto e exam sem regra de negócio, fiz na mesma branch porque tenho dislexia, sorry!

--- src/main/kotlin/trabalho/trabalhojedersonbackend/services/impl/ClinicServiceImpl.kt
+++ src/main/kotlin/trabalho/trabalhojedersonbackend/services/impl/ClinicServiceImpl.kt
@@ -2,19 +2,18 @@ package trabalho.trabalhojedersonbackend.services.impl
 
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
-import trabalho.trabalhojedersonbackend.exceptions.UpdateClinicException
 import trabalho.trabalhojedersonbackend.model.Clinic
 import trabalho.trabalhojedersonbackend.repositories.ClinicRepository
 import trabalho.trabalhojedersonbackend.services.ClinicService
 
 @Service
 class ClinicServiceImpl(val clinicRepository: ClinicRepository, val clinic: Clinic) : ClinicService {
-    @Throws (UpdateClinicException::class)
+
     override fun findAll(): List<Clinic>? = clinicRepository.findAll()
 
     override fun findById(id: Long): Clinic? = clinicRepository.findByIdOrNull(id)
 
     override fun save(clinic: Clinic) = clinicRepository.save(clinic)
 
-    override fun deleteById(id: Long): Clinic? = clinicRepository.findByIdOrNull(id)
+    override fun deleteById(id: Long) = clinicRepository.deleteById(id)
 }
