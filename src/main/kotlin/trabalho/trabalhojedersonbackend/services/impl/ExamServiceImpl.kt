69c22e77a61c138d40d54d480bac057a7403e8cb
idd_vsilva <idd_vsilva@uolinc.com>
2020-06-20 15:49:15 -0300

    Fixed queries to find by the user logged in

--- src/main/kotlin/trabalho/trabalhojedersonbackend/services/impl/ExamServiceImpl.kt
+++ src/main/kotlin/trabalho/trabalhojedersonbackend/services/impl/ExamServiceImpl.kt
@@ -3,6 +3,7 @@ package trabalho.trabalhojedersonbackend.services.impl;
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service;
 import trabalho.trabalhojedersonbackend.enums.ExamStatusEnum
+import trabalho.trabalhojedersonbackend.enums.UserTypeEnum
 import trabalho.trabalhojedersonbackend.exceptions.BadRequestException
 import trabalho.trabalhojedersonbackend.exceptions.ExamAlreadyAnalisedException
 import trabalho.trabalhojedersonbackend.model.Exam
@@ -10,6 +11,7 @@ import trabalho.trabalhojedersonbackend.repositories.ExamRepository
 import trabalho.trabalhojedersonbackend.services.ExamService
 import java.time.LocalDateTime
 import javax.persistence.EntityNotFoundException
+import kotlin.system.exitProcess
 
 @Service
 class ExamServiceImpl(val examRepository: ExamRepository) : ExamService {
@@ -18,20 +20,75 @@ class ExamServiceImpl(val examRepository: ExamRepository) : ExamService {
 
     override fun findById(id: Long): Exam? = examRepository.findByIdOrNull(id)
 
-    //TODO BUSCA POR NOME, ACHO QUE TEM BUSCAR PELA PESSOA
-    //POR STATUS TESTEI AQUI E TÁ FUNCIONANDO
-    override fun findFiltered(cpf: String?, status: ExamStatusEnum?): List<Exam>? {
-        return if (/*name.isNullOrBlank() &&*/ status == null) throw BadRequestException("Nome ou status deve ser preenchido")
-//        else if(name!!.isNotBlank() && status!!.isNotBlank()) examRepository.findByNameAndStatus(name, status)
-//        else if (name!!.isNotBlank()) examRepository.findByName(name)
-        else examRepository.findByStatus(status)
+    override fun findUserAllExams(userType: UserTypeEnum, userId: Long): List<Exam> {
+        return when (userType) {
+            UserTypeEnum.CLINIC -> {
+                findAllByClinicId(userId)
+            }
+            UserTypeEnum.DOCTOR -> {
+                findAllByDoctorId(userId)
+            }
+            UserTypeEnum.PATIENT -> {
+                findAllByPatientId(userId)
+            }
+        }
     }
 
-    //patient + status tá funcionando
-    override fun findPatientExamsFiltered(patientId: Long, type: String?, status: ExamStatusEnum?): List<Exam>? {
-        return examRepository.findByStatusAndPatientId(status!!,patientId)
+    override fun findAllByPatientId(patientId: Long): List<Exam> {
+        return examRepository.findByPatientId(patientId).let {
+            if (it.isEmpty()) throw EntityNotFoundException()
+            else it
+        }
     }
 
+    override fun findAllByClinicId(clinicId: Long): List<Exam> {
+        return examRepository.findByClinicId(clinicId).let {
+            if(it.isEmpty()) throw EntityNotFoundException()
+            else it
+        }
+    }
+
+    override fun findAllByDoctorId(doctorId: Long): List<Exam> {
+        return examRepository.findByDoctorId(doctorId).let {
+            if(it.isEmpty()) throw EntityNotFoundException()
+            else it
+        }
+    }
+
+    override fun findUserExamsByStatus(userType: UserTypeEnum, userId: Long, status: ExamStatusEnum): List<Exam> {
+        return when (userType) {
+            UserTypeEnum.CLINIC -> {
+                findClinicExamsByStatus(userId,status)
+            }
+            UserTypeEnum.DOCTOR -> {
+                findDoctorExamsByStatus(userId,status)
+            }
+            UserTypeEnum.PATIENT -> {
+                findPatientExamsByStatus(userId,status)
+            }
+        }
+    }
+
+    override fun findPatientExamsByStatus(patientId: Long, status: ExamStatusEnum): List<Exam> {
+        return examRepository.findByStatusAndPatientId(status, patientId).let {
+            if (it.isEmpty()) throw EntityNotFoundException()
+            else it
+        }
+    }
+
+    override fun findClinicExamsByStatus(clinicId: Long, status: ExamStatusEnum): List<Exam> {
+        return examRepository.findByStatusAndClinicId(status, clinicId).let {
+            if (it.isEmpty()) throw EntityNotFoundException()
+            else it
+        }
+    }
+
+    override fun findDoctorExamsByStatus(doctorId: Long, status: ExamStatusEnum): List<Exam> {
+        return examRepository.findByStatusAndDoctorId(status, doctorId).let {
+            if (it.isEmpty()) throw EntityNotFoundException()
+            else it
+        }
+    }
 
     override fun save(exam: Exam): Exam? {
         exam.status = ExamStatusEnum.EXAME_EM_ANDAMENTO
